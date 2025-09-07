e0524457af4a41d873d5b7a921c7dee56123753a
Praveen C <cpraveen@gmail.com>
2025-09-07 15:17:43 +0530

    Add vtk, improve makefile

--- poisson/main.c
+++ poisson/main.c
@@ -1,5 +1,6 @@
 #include "utils.h"
 #include "poisson.h"
+#include "vtk.h"
 
 #define LEVEL  8
 
@@ -9,6 +10,15 @@ u[left]   = dirichlet(0);
 u[right]  = dirichlet(0);
 u[bottom] = dirichlet(0);
 u[top]    = dirichlet(0);
+
+void mg_print (mgstats mg)
+{
+   if (mg.i > 0 && mg.resa > 0.)
+      printf ("Iter=%d, ||f||=%g, ||u||=%g, %g, nrelax=%d\n", 
+              mg.i, mg.resb, mg.resa,
+              mg.resb > 0 ? exp (log (mg.resb/mg.resa)/mg.i) : 0.0,
+              mg.nrelax);
+}
 
 int main()
 {
@@ -20,6 +30,11 @@ int main()
       f[] = -1.0;
    }
 
-   mgstats mgu = poisson(u, f);
+   mgstats mgu = poisson(u, f, tolerance=1e-6);
+   mg_print(mgu);
    output_ppm(u, file="u.png", n=1024);
+   FILE* fp = fopen("u.vtk", "w");
+   output_vtk({u}, 1<<LEVEL, fp, true);
+   fclose(fp);
 }
+
